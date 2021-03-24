c03db93ba3058d500a3cee89060eb51fa7796afc
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-03-24 10:36:30 -0400

    New app project

---
+++ MyBunch/MyBunchApp/MyBunchApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MyBunchApp.ViewModels;
+using MyBunchApp.Views;
+using Xamarin.Forms;
+
+namespace MyBunchApp
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+			Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+		}
+
+	}
+}
---
+++ MyBunch/MyBunchApp/MyBunchApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using MyBunchApp.ViewModels;
+
+namespace MyBunchApp.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
