abf90ae784f490eafb6af91ae19665b66c3c0037
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-07-28 16:27:27 +0200

    Update namespace

--- ProjectSparrow/SparrowServer/Models/Account.cs
+++ ProjectSparrow/SparrowServer/Models/Account.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SparrowServer.Server.Models
+namespace SparrowServer.Models
 {
     public class AccountCredentials
     {
