4e501a571e94d6447dc540bfb7586b8b788c4202
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-07-28 16:25:17 +0200

    Add basic account models

---
+++ ProjectSparrow/SparrowServer/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparrowServer.Server.Models
+{
+    public class AccountCredentials
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class AccountDetails
+    {
+        public ulong ID { get; set; }
+        
+        public string[] Interests { get; set; }
+    }
+
+}
