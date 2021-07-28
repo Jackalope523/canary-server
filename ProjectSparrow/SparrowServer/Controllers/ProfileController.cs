03bab99cada55b8a0d2109849a580c18a9c2dfce
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-07-28 15:13:48 +0200

    Add profile routing skeleton

---
+++ ProjectSparrow/SparrowServer/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparrowServer.Controllers
+{
+    [Route("accounts/[controller]")]
+    [ApiController]
+    public class ProfileController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetPage()
+        {
+
+            return Ok();
+
+            // return View(); // Can mess with this at a later date.
+        }
+
+    }
+
+}
