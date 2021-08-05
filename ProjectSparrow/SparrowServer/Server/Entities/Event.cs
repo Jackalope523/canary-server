671104c9edef34d73fb86e4f6e506fa539393661
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-08-05 21:37:50 +0200

    Basic Event data

--- ProjectSparrow/SparrowServer/Server/Entities/Event.cs
+++ ProjectSparrow/SparrowServer/Server/Entities/Event.cs
@@ -7,5 +7,31 @@ namespace SparrowServer.Server.Entities
 {
     public class Event
     {
+        #region Variables
+
+        public string EventID { get; init; }
+        public string HostID { get; init; }
+        // Location class needed
+        public DateTime StartTime { get; init; }
+        public IList<Participant> Participants { get; } // Head banger
+        // Event Status data type needed
+
+        #endregion
+
+        public void AddParticipant(string participantID)
+        {
+            // Processing
+        }
+
+        public void RemoveParticipant(string participantID)
+        {
+            // Processing
+        }
+    }
+
+    public struct Participant
+    {
+        public string ID;
+        public DateTime JoinedTime;
     }
 }
