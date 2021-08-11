33c4a7c53bae1ad6f2579a3b8f799f03566b3d24
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-08-11 11:49:40 +0200

    Flesh out Participant, add PastParticipant, add Lists

--- ProjectSparrow/SparrowServer/Server/Entities/Event.cs
+++ ProjectSparrow/SparrowServer/Server/Entities/Event.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Immutable;
 
 namespace SparrowServer.Server.Entities
 {
@@ -13,10 +14,15 @@ namespace SparrowServer.Server.Entities
         public string HostID { get; init; }
         // Location class needed
         public DateTime StartTime { get; init; }
-        public IList<Participant> Participants { get; } // Head banger
+
+        public IList<Participant> Participants => ImmutableList.CreateRange(Participants.ToList());
+
+        private SortedSet<Participant> currentParticipants = new(Participant.CompareID);
+        private List<PastParticipant> participantHistory = new();
         // Event Status data type needed
 
         #endregion
+
 
         public void AddParticipant(string participantID)
         {
@@ -27,11 +33,39 @@ namespace SparrowServer.Server.Entities
         {
             // Processing
         }
+
     }
 
-    public struct Participant
+    public struct Participant : IComparable<Participant>
     {
-        public string ID;
-        public DateTime JoinedTime;
+        public static IComparer<Participant> CompareID => Comparer<Participant>.Create((participantA, participantB) => string.Compare(participantA.ID, participantB.ID));
+
+        public readonly string ID;
+        public readonly DateTime JoinedTime;
+
+        public Participant(string userID)
+        {
+            ID = userID;
+            JoinedTime = DateTime.Now.ToUniversalTime();
+        }
+
+        public int CompareTo(Participant other)
+        {
+            return ID.CompareTo(other.ID);
+        }
+    }
+
+    struct PastParticipant
+    {
+        public readonly string ID;
+        public readonly DateTime JoinedTime;
+        public readonly DateTime LeftTime;
+
+        public PastParticipant(Participant participant)
+        {
+            ID = participant.ID;
+            JoinedTime = participant.JoinedTime;
+            LeftTime = DateTime.Now.ToUniversalTime();
+        }
     }
 }
