0550e047259d924eef62d72e12a9b8751d74806c
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-08-13 21:57:44 +0200

    Add basic Account class

--- ProjectSparrow/Server/Controls/AccountManager.cs
+++ ProjectSparrow/Server/Controls/AccountManager.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace Server.Entities
+namespace Server.Controls
 {
-    internal class User : Account
+    internal class AccountManager
     {
     }
 }
---
+++ ProjectSparrow/Server/Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Server.Entities
+{
+    internal abstract class Account
+    {
+        public string UniqueID { get; }
+        public string Identification { get; }
+
+        private readonly string passkey;
+
+        protected Account()
+        {
+
+        }
+
+        public bool VerifyPasskey(string otherPasskey)
+        {
+            bool isValid = true;
+
+            for (int i = 0; i < otherPasskey.Length && i < passkey.Length; i++)
+            {
+                if (otherPasskey[i] != passkey[i])
+                {
+                    isValid = false;
+                }
+            }
+
+            return isValid;
+        }
+
+    }
+}
