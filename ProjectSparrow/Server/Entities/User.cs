78cd78aaa4b82017c93bedcad93dd7cacfeb138b
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-08-14 16:43:58 +0200

    Add User details and Interest enum

--- ProjectSparrow/Server/Entities/User.cs
+++ ProjectSparrow/Server/Entities/User.cs
@@ -7,5 +7,25 @@ namespace Server.Entities
 {
     internal class User : Account
     {
+        public string Name { get; init; }
+        public DateTime DateOfBirth { get; init; }
+        public Interest Interests { get; set; }
+
+        public DateTime JoinDate { get; init; }
+
+        public string CurrentEventID { get; private set; }
+
+    }
+
+    [Flags]
+    internal enum Interest : ushort
+    {
+        None = 0,
+        Sports = 1,
+        Cars = 2,
+        Socials = 4,
+        Parties = 8,
+
+        Everything = 0xFFFF
     }
 }
