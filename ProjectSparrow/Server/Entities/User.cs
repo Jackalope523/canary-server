803ea07b882632265f38de2126fb1dab1ec8067d
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-08-14 20:55:16 +0200

    Add simple Reputation struct

--- ProjectSparrow/Server/Entities/User.cs
+++ ProjectSparrow/Server/Entities/User.cs
@@ -12,6 +12,8 @@ namespace Server.Entities
         public Interest Interests { get; set; }
 
         public DateTime JoinDate { get; init; }
+        public Reputation GoerReputation { get; private set; }
+        public Reputation HostReputation { get; private set; }
 
         public string CurrentEventID { get; private set; }
 
@@ -25,6 +27,7 @@ namespace Server.Entities
         }
     }
 
+
     [Flags]
     internal enum Interest : ushort
     {
@@ -36,4 +39,38 @@ namespace Server.Entities
 
         Everything = 0xFFFF
     }
+
+
+    internal struct Reputation
+    {
+        public ushort ReputationScore { get; }
+        public ReputationTier ReputationTier 
+        { 
+            get
+            {
+                foreach (ReputationTier tier in Enum.GetValues<ReputationTier>().Reverse())
+                {
+                    if (ReputationScore > (ushort)tier)
+                    {
+                        return tier;
+                    }
+                }
+
+                return ReputationTier.Poor;
+            }
+        }
+    }
+
+
+    internal enum ReputationTier : ushort
+    {
+        Poor = 0,
+        Mediocre = 200,
+        Normal = 400,
+        Good = 600,
+        Great = 800,
+        Excellent = 900
+    }
+
+
 }
