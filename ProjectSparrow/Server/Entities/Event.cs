331b336b86e05532733a0200e232a0c6c13b5a51
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2022-01-25 21:45:32 -0500

    Encapsulate participant data into log class

--- ProjectSparrow/Server/Entities/Event.cs
+++ ProjectSparrow/Server/Entities/Event.cs
@@ -17,20 +17,11 @@ namespace Server.Entities
         public EventType EventType { get; init; }
         public DateTime StartTime { get; private init; }
 
-        public IList<Participant> Participants => ImmutableList.CreateRange(currentParticipants.ToList());
+        public IList<Participant> Participants => ImmutableList.CreateRange(participantLog.activeParticipants.ToList());
 
-        // Event Status data type needed
+        // Event Status data type needed. e.g. gaining popularity
 
-        /*
-         * Idea for new participant tracking/history system.
-         * Single list with each participant, past or present.
-         * Each participant has an array of tuples representing an 'action' (joined, left) and a timestamp.
-         * Can have a parameter that returns all active participants, otherwise just give them the entire dump.
-         * This allows for participants to leave and rejoin without object duplication. It also allows for any future actions to be integrated seamlessly.
-         */
-
-        private readonly SortedSet<Participant> currentParticipants = new();
-        private readonly List<PastParticipant> participantHistory = new();
+        private readonly ParticipantLog participantLog;
 
         #endregion
 
@@ -38,30 +29,59 @@ namespace Server.Entities
         {
             HostID = hostID;
 
-            Participant host = new(hostID);
-            currentParticipants.Add(host);
+            participantLog = new();
+            ParticipantJoined(hostID);
 
             StartTime = DateTime.UtcNow;
         }
 
-        public void AddParticipant(string participantID)
+        public void ParticipantJoined(string participantID)
         {
-            Participant participant = new(participantID);
-
-            currentParticipants.Add(participant);
+            participantLog.AddParticipant(participantID);
         }
+
+        public void ParticipantLeft(string participantID)
+        {
+            participantLog.RemoveParticipant(participantID);
+		}
+
+	}
+
+	internal class ParticipantLog
+	{
+        private Dictionary<string, LinkedList<ParticipantAction>> log;
+
+        public IList<Participant> activeParticipants => null; // TODO
+
+        public ParticipantLog()
+		{
+            log = new Dictionary<string, LinkedList<ParticipantAction>>();
+		}
+
+        public void AddParticipant(string participantID)
+		{
+            if (!log.ContainsKey(participantID))
+			{
+                log.Add(participantID, new LinkedList<ParticipantAction>());
+			}
+
+            log[participantID].AddLast(new ParticipantAction(PerformedAction.Joined));
+		}
 
         public void RemoveParticipant(string participantID)
         {
-            Participant participant = currentParticipants.ToList().Find(x => x.ID.Equals(participantID));
-
-            currentParticipants.Remove(participant);
-
-            PastParticipant pastParticipant = new(participant);
-
-            participantHistory.Add(pastParticipant);
+            if (log.ContainsKey(participantID))
+            {
+                log[participantID].AddLast(new ParticipantAction(PerformedAction.Left));
+            }
         }
 
+        public float ParticipantActivityRatio(float timeInMinutes = 15)
+		{
+            // TODO
+            // Returns 1 if participants left to participants joined is equal. <1 if more left, >1 if more joined.
+            return 0;
+		}
     }
 
 
@@ -82,18 +102,19 @@ namespace Server.Entities
         }
     }
 
+    
+    internal readonly struct ParticipantAction
+	{
+        public DateTime Time { get; }
+        public PerformedAction Type { get; }
 
-    readonly struct PastParticipant
-    {
-        public string ID { get; }
-        public DateTime JoinedTime { get; }
-        public DateTime LeftTime { get; }
+        public ParticipantAction(PerformedAction actionType)
+		{
+            Time = DateTime.Now;
+            Type = actionType;
+		}
+	}
 
-        public PastParticipant(Participant participant)
-        {
-            ID = participant.ID;
-            JoinedTime = participant.JoinedTime;
-            LeftTime = DateTime.UtcNow;
-        }
-    }
+
+    internal enum PerformedAction { Joined, Left }
 }
