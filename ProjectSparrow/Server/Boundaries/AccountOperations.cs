04984c7a3b2bba746ea413b57a01b89580895be2
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2023-01-02 17:27:29 -0500

    Add desired account system features

---
+++ ProjectSparrow/Server/Boundaries/AccountOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Boundaries
+{
+	public interface IAccountOperations
+	{
+		public void GetUserProfile(string identification, string targetIdentification);
+
+		public void CreateUser(string identification, string name, DateTime dateOfBirth);
+		public void EditUser(string identification);
+		public void DeleteUser(string identification);
+
+		public void UpdatePhoto(string identification);
+
+		public void GetFollowedUsers(string identification);
+		public void GetBlockedUsers(string identification);
+
+		public void FollowUser(string identification, string targetIdentification);
+		public void UnfollowUser(string identification, string targetIdentification);
+		public void BlockUser(string identification, string targetIdentification);
+		public void UnblockUser(string identification, string targetIdentification);
+
+	}
+}
