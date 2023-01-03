8cf23eb2b2ffe4c44df500b95d3918dc6f3c577d
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2023-01-02 19:06:11 -0500

    Add user following and blocking scaffold

--- ProjectSparrow/Web/Controllers/ProfileController.cs
+++ ProjectSparrow/Web/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Server.Boundaries;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,56 @@ namespace Web.Controllers
     [Route("accounts/[controller]")]
     [ApiController]
     public class ProfileController : Controller
-    {
+	{
+		private IAccountOperations accounts;
 
-        [HttpGet]
+		public ProfileController(IAccountOperations accountOperations)
+		{
+			accounts = accountOperations;
+		}
+
+		[HttpGet]
         public IActionResult GetPage()
         {
 
             return Ok();
-
-            // return View(); // Can mess with this at a later date.
         }
 
-    }
+        [HttpGet("following")]
+        public IActionResult GetFollowed()
+        {
+            return Ok();
+        }
+
+		[HttpPost("following")]
+		public IActionResult FollowUser()
+		{
+			return Ok();
+		}
+
+		[HttpPut("following")]
+		public IActionResult UnfollowUser()
+		{
+			return Ok();
+		}
+
+		[HttpGet("blocked")]
+		public IActionResult GetBlocked()
+		{
+			return Ok();
+		}
+
+		[HttpPost("blocked")]
+		public IActionResult BlockUser()
+		{
+			return Ok();
+		}
+
+		[HttpPut("blocked")]
+		public IActionResult UnblockUser()
+		{
+			return Ok();
+		}
+	}
 
 }
