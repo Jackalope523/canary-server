874edf831fdf2bb822a276cfa4c1964ed7792a02
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2023-01-05 18:58:23 -0500

    Add Event control skeleton

---
+++ ProjectSparrow/Server/Controls/EventManager.cs
@@ -0,0 +1,51 @@
+﻿using Server.Boundaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controls
+{
+	public class EventManager : IEventOperations
+	{
+		public void GetEventInformation(string identification, string eventID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<string> GetEventsInArea(string identification, float latitude, float longitude, float distance)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<string> GetPersonalisedEventsInArea(string identification, float latitude, float longitude, float distance)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CreateEvent(string identification, float latitude, float longitude)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void JoinEvent(string identification, string eventID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LeaveEvent(string identification)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void EndEvent(string identification)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<string> GetAttendees(string identification, string eventID)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
