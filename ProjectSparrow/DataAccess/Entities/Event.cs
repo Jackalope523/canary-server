11dc623bda68a0989b21f2a740159335cb0ddff2
EthanCote5233 <ECote523@Gmail.com>
2023-01-05 19:48:13 -0500

    Begin Event entity

---
+++ ProjectSparrow/DataAccess/Entities/Event.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace DataAccess.Entities
+{
+    internal class Event
+    {    
+        private int Id;
+        private User Host { get; set; }
+
+        private List<EventLink> Links;
+
+        
+
+
+        public GeoLocation Location { get; }
+        public EventType EventType { get; init; }
+        public DateTime StartTime { get; private init; }
+
+        public IList<Participant> Participants => ImmutableList.CreateRange(participantLog.ActiveParticipants.ToList());
+
+        // Event Status data type needed. e.g. gaining popularity
+
+        private readonly ParticipantLog participantLog;
+
+        public Event(string hostID)
+        {
+            HostID = hostID;
+
+            participantLog = new();
+            ParticipantJoined(hostID);
+
+            StartTime = DateTime.UtcNow;
+        }
+
+        public void ParticipantJoined(string participantID)
+        {
+            participantLog.AddParticipant(participantID);
+        }
+
+        public void ParticipantLeft(string participantID)
+        {
+            // TODO Host leaves, too few participants, only host, etc
+
+
+            participantLog.RemoveParticipant(participantID);
+        }
+
+        public bool TransferOwnership()
+        {
+            // TODO
+
+            return true;
+        }
+
+    }
+}
