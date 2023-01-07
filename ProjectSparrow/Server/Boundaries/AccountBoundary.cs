1d987e5cc74ff072db545364ce2eca4474bfff73
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2023-01-07 16:59:14 -0500

    Add more control logic and database functions

--- ProjectSparrow/Server/Boundaries/AccountBoundary.cs
+++ ProjectSparrow/Server/Boundaries/AccountBoundary.cs
@@ -8,26 +8,37 @@ using System.Threading.Tasks;
 namespace Server.Boundaries
 {
 	public record ThinAccount(string AccountID, string Identification);
-	public record ThinUser(string AccountID, string Identification, string Name, DateTime DateOfBirth, string ProfilePhoto) : ThinAccount(AccountID, Identification);
+	public record ThinUser(string AccountID, string Identification, string Name, DateTime DateOfBirth, string ProfilePhoto, int Reputation, int NumberOfFollowers)
+		: ThinAccount(AccountID, Identification);
 
-	public record ThinListUser(string UserID, string Name, string ProfilePhoto);
+	public record ThinListUser(string AccountID, string Name, string ProfilePhoto);
+
+	public record ThinProfile(string AccountID, string Name, string ProfilePhoto, int Reputation, int NumberOfFollowers);
 
 	public interface IAccountDatabase
 	{
 		ThinAccount GetAccount(string accountID);
 		ThinAccount FindAccount(string identification);
 
-		void UpdateAccount(string accountID, string newIdentification, string newHashedPasskey);
+		void UpdateAccount(string accountID, string newIdentification, string newPasskey);
 		void DeleteAccount(string accountID);
 
 		ThinUser GetUser(string accountID);
-		ThinUser CreateUser(string identification, string hashedPasskey, string name, DateTime dateOfBirth, string profilePhoto);
-		void UpdateUser(string userID, string newName, DateTime newDateOfBirth, string newPhoto);
+		ThinUser CreateUser(string identification, string passkey, string name, DateTime dateOfBirth, string profilePhoto);
+		void UpdateUser(string accountID, string newName, DateTime newDateOfBirth, string newPhoto);
+
+		List<ThinListUser> GetFollowedUsers(string accountID);
+		List<ThinListUser> GetBlockedUsers(string accountID);
+
+		void FollowUser(string accountID, string targetAccountID);
+		void UnfollowUser(string accountID, string targetAccountID);
+		void BlockUser(string accountID, string targetAccountID);
+		void UnblockUser(string accountID, string targetAccountID);
 	}
 
 	public interface IAccountOperations
 	{
-		ThinUser GetUserProfile(string identification, string targetIdentification);
+		ThinProfile GetUserProfile(string identification, string targetIdentification);
 
 		string TryLogin(string identification, string passkey);
 		void CreateUser(string identification, string passkey, string name, DateTime dateOfBirth, string profilePhoto);
