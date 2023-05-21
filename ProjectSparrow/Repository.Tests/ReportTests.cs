00bb7a8df7b76ab2088acd0e047b99029e67f3cd
EthanCote5233 <ECote523@Gmail.com>
2023-05-21 09:48:27 -0400

    Add Report Tests

---
+++ ProjectSparrow/Repository.Tests/ReportTests.cs
@@ -0,0 +1,39 @@
+﻿using Server.Boundaries;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Tests
+{
+    public class ReportTests
+    {
+        [Fact]
+        public void GetReports_SUCCESS()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void GetReportsByUser_SUCCESS()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void ReportUser_SUCCESS()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void GetEventReports_SUCCESS()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void ReportEvent_SUCCESS()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
