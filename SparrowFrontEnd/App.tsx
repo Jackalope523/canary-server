c700407fed2fd4743ba76ce0f23e739e833fc650
EthanCote5233 <ECote523@Gmail.com>
2023-07-09 13:18:20 -0400

    Rebuild SparrowFrontEnd

--- SparrowFrontEnd/App.tsx
+++ SparrowFrontEnd/App.tsx
@@ -1,96 +1,118 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-import PortalScreen from './src/features/portal/screens/PortalScreen';
-import LoginScreen from './src/features/portal/screens/LoginScreen';
-import VerifyScreen from './src/features/portal/screens/VerifyScreen';
-import RegisterScreen from './src/features/portal/screens/RegisterScreen';
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
 
-import FeedScreen from './src/features/events/screens/FeedScreen';
-import DiscoverScreen from './src/features/events/screens/DiscoverScreen';
-import ProfileScreen from './src/features/profile/screens/ProfileScreen';
-import { Provider } from 'react-redux';
-
-export type RootStackParamList = {
-  Portal: undefined;
-  Authentication: undefined;
-  Login: undefined;
-  Verify: { phoneNumber: string};
-  Register: { phoneNumber: string};
-  Landing: undefined;
-};
-
-export type RootTabsParamList = {
-  Feed: undefined;
-  Discover: undefined;
-  Profile: undefined;
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
-const Tabs  = createBottomTabNavigator<RootTabsParamList>();
-
-export default function App(): JSX.Element {
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Portal">
-        <Stack.Screen name="Portal" component={PortalScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Authentication" component={Authentication}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Landing" component={Landing}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
 }
 
-const Authentication = () => {
-  return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen name="Verify" component={VerifyScreen}
-        options={{
-          title: 'Verify',
-          headerStyle: {
-            backgroundColor: '#307ecc',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-      <Stack.Screen name="Register" component={RegisterScreen}
-        options={{
-          title: 'Register',
-          headerStyle: {
-            backgroundColor: '#307ecc',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
-};
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
 
-const Landing = () => {
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
-    <Tabs.Navigator initialRouteName='Discover'
-      screenOptions={{ headerShown: false }}>      
-        <Tabs.Screen name='Feed' component={FeedScreen} />
-        <Tabs.Screen name='Discover' component={DiscoverScreen} />
-        <Tabs.Screen name='Profile' component={ProfileScreen} />
-    </Tabs.Navigator>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
