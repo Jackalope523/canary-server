7cddd01e47489d6edd52804b40974f9685c05eb8
Anna <teesannalv@gmail.com>
2023-07-27 18:26:08 +0300

    Add search bar.

--- SparrowFrontEnd/src/screens/Discovery.tsx
+++ SparrowFrontEnd/src/screens/Discovery.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import SearchBar from '../components/molecules/SearchBar';
+import { globalStyles } from '../styles/Global';
 
 export default function DiscoveryScreen({ navigation }) {
     return (
         <View>
             <Text onPress={() => navigation.navigate('Activity')}>Discovery Screen</Text>
+            <SearchBar />
+            <TouchableOpacity style={[globalStyles.textButtonExtraSmall, globalStyles.textButtonPrimary]}>
+                <Text style={globalStyles.textButtonExtraSmallText}>Create Event</Text>
+            </TouchableOpacity>
         </View>
     );
 };
