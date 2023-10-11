06dd236f6afdfffd4bfe15ea687fc394cde8372c
annaozola <teesannalv@gmail.com>
2023-10-10 22:50:37 -0400

    Add slider file

---
+++ SparrowFrontEnd/src/components/atoms/slider.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const slider = (props: Props) => {
+  return (
+    <View>
+      <Text>slider</Text>
+    </View>
+  )
+}
+
+export default slider
+
+const styles = StyleSheet.create({})
