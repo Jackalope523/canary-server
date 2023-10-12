2f7e821823e5220e17878fde1ed3d53bbb602a53
annaozola <teesannalv@gmail.com>
2023-10-12 17:35:51 -0400

    Update rangeSelector

--- SparrowFrontEnd/src/components/atoms/rangeSelector.tsx
+++ SparrowFrontEnd/src/components/atoms/rangeSelector.tsx
@@ -1,24 +1,57 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native'
 import React from 'react'
 import { Colors } from '../../styles/Colors'
-import Animated, {useAnimatedGestureHandler, useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
+import Animated, {useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, useAnimatedProps, runOnJS} from 'react-native-reanimated';
 import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
 
+
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 // TODO replace -40 with real padding size or make the responsiveness better
 const screenWidth = Dimensions.get('screen').width - 40;
 const knobSize = 20;
+const maxWidth = screenWidth - knobSize / 2 + 6
 
 const RangeSelector = ({min, max, steps, onValueChange}) => {
   
   const xKnob1 = useSharedValue(0);
+  const scaleKnob1 = useSharedValue(1);
+
+  const xKnob2 = useSharedValue(maxWidth);
+  const scaleKnob2 = useSharedValue(1);
   
   const gestureHandler1 = useAnimatedGestureHandler ({
-    onStart: (_, ctx) => {},
-    onActive: (event, ctx) => {
-      xKnob1.value = event.translationX;
+    onStart: (_, ctx) => {
+      ctx.startX = xKnob1.value;
     },
-    onEnd: () => {},
+    onActive: (event, ctx) => {
+      scaleKnob1.value = 1.3;
+      xKnob1.value = ctx.startX + event.translationX < 0 ? 0 : ctx.startX + event.translationX > maxWidth ? maxWidth : ctx.startX + event.translationX;
+    },
+    onEnd: () => {
+      scaleKnob1.value = 1;
+      runOnJS(onValueChange)({
+        min: `${Math.round((min + (xKnob1.value / maxWidth) * (max - min)) / steps ) * steps }`,
+        max: `${Math.round((min + (xKnob2.value / maxWidth) * (max - min)) / steps ) * steps }`,
+      });
+    },
+  });
+
+  const gestureHandler2 = useAnimatedGestureHandler ({
+    onStart: (_, ctx) => {
+      ctx.startX = xKnob2.value;
+    },
+    onActive: (event, ctx) => {
+      scaleKnob2.value = 1.3;
+      xKnob2.value = ctx.startX + event.translationX < xKnob1.value ? xKnob1.value : ctx.startX + event.translationX > maxWidth ? maxWidth : ctx.startX + event.translationX;
+    },
+    onEnd: () => {
+      scaleKnob2.value = 1;
+      runOnJS(onValueChange)({
+        min: `${Math.round((min + (xKnob1.value / maxWidth) * (max - min)) / steps ) * steps }`,
+        max: `${Math.round((min + (xKnob2.value / maxWidth) * (max - min)) / steps ) * steps }`,
+      });
+    },
   });
 
   const styleLine = useAnimatedStyle(() => {
@@ -27,8 +60,8 @@ const RangeSelector = ({min, max, steps, onValueChange}) => {
       height: 3,
       marginTop: -3,
       borderRadius: 3,
-      width: 100,
-      transform: [{ translateX: 0 }],
+      width: xKnob2.value - xKnob1.value,
+      transform: [{ translateX: xKnob1.value }],
     }
   });
 
@@ -37,8 +70,36 @@ const RangeSelector = ({min, max, steps, onValueChange}) => {
       transform: [
         {
           translateX: xKnob1.value,
-        }
+        },
+        {
+          scale: scaleKnob1.value,
+        },
       ]
+    }
+  });
+
+  const styleKnob2 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: xKnob2.value,
+        },
+        {
+          scale: scaleKnob2.value,
+        },
+      ]
+    }
+  });
+
+  const propsLabel1 = useAnimatedProps(() => {
+    return {
+      text: `${Math.round((min + (xKnob1.value / maxWidth) * (max - min)) / steps ) * steps }`,
+    }
+  });
+
+  const propsLabel2 = useAnimatedProps(() => {
+    return {
+      text: `${Math.round((min + (xKnob2.value / maxWidth) * (max - min)) / steps ) * steps }`,
     }
   });
 
@@ -46,15 +107,20 @@ const RangeSelector = ({min, max, steps, onValueChange}) => {
     <View>
       <View style={styles.rangeWrapper}>
         <View style={styles.labelsWrapper}>
-          <Text style={styles.label}>{min}</Text>
-          <Text style={styles.label}>{max}</Text>
+          <AnimatedTextInput defaultValue={'0'} editable={false} style={styles.label} animatedProps={propsLabel1} />
+          <AnimatedTextInput defaultValue={'0'} editable={false} style={styles.label} animatedProps={propsLabel2} />
         </View>
         <View style={styles.track} />
         <Animated.View style={styleLine} />
         <View>
+
+          {/* TODO make flex: 1 style work for gesturehandlerrootview */}
           <GestureHandlerRootView>
             <PanGestureHandler onGestureEvent={gestureHandler1}>
               <Animated.View style={[styles.knob, styleKnob1]} />
+            </PanGestureHandler>
+            <PanGestureHandler onGestureEvent={gestureHandler2}>
+              <Animated.View style={[styles.knob, styleKnob2]} />
             </PanGestureHandler>
           </GestureHandlerRootView>
         </View>
@@ -67,6 +133,7 @@ export default RangeSelector
 
 const styles = StyleSheet.create({
   // From tutorial - change when functional
+
   rangeWrapper: {
     backgroundColor: Colors.fuchsia200,
 
