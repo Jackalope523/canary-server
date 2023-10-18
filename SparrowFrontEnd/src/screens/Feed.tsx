cb12330e2b3c220ba18c482bbfe3a1c493ff884f
annaozola <teesannalv@gmail.com>
2023-10-18 08:28:41 +0200

    Code cleanup

--- SparrowFrontEnd/src/screens/Feed.tsx
+++ SparrowFrontEnd/src/screens/Feed.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, ImageBackground} from 'react-native';
+import React from 'react';
+import { View, StyleSheet} from 'react-native';
 
 // Testing components
 import { Spacing } from '../styles/Spacing';
