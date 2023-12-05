e4141a7bd11dc38aade16f54f37bafb7b8aaa304
annaozola <teesannalv@gmail.com>
2023-12-05 15:26:55 +0200

    Fix active Button

--- SparrowFrontEnd/src/components/Button.tsx
+++ SparrowFrontEnd/src/components/Button.tsx
@@ -216,31 +216,17 @@ export const Button: React.FC<ButtonProps> = ({
   // ! ||                                   Button                                       ||
   // ! ||--------------------------------------------------------------------------------||
 
-  const [isPressed, setIsPressed] = React.useState(false);
+  // TODO button needs to reset state back to rest (isPressed = false), when the user has
+  // left the screen / doesn't see the button anymore.
 
-  const handlePressIn = () => {
-    if (self != null && status != null && changeState != null) {
-      if (status == self) changeState(-1);
-      else changeState(self);
-    } else {
-      setIsPressed(true);
-      if (onPress != null) {
-        onPress();
-      }
-    }
-  };
+  const [isPressed, setIsPressed] = React.useState(false);
 
   return (
     <Pressable
-      onPress={handlePressIn}
-      style={[
-        self == status || isPressed ? btnActiveStyle : btnStyle,
-        styles.btnBase,
-      ]}
+      onPress={() => setIsPressed(!isPressed)}
+      style={isPressed ? btnStyle : btnActiveStyle}
       disabled={disabled}>
-      {btnIcon && <Icon style={btnIconStyle} name={btnIcon} />}
-      <Text
-        style={self == status || isPressed ? btnActiveTextStyle : btnTextStyle}>
+      <Text style={isPressed ? btnTextStyle : btnActiveTextStyle}>
         {btnText}
       </Text>
     </Pressable>
