429a6a7312bdb3ad6d93e79da5dc095a35a5152a
annaozola <teesannalv@gmail.com>
2023-12-05 15:58:36 +0200

    Refactor Avatars

--- SparrowFrontEnd/src/styles/Avatars.tsx
+++ SparrowFrontEnd/src/styles/Avatars.tsx
@@ -1,52 +1,52 @@
-import { StyleSheet } from "react-native";
-import { Colors } from "./Colors";
+import { StyleSheet } from 'react-native';
+import { Colors } from './Colors';
 
 export const avatarStyles = StyleSheet.create({
-    // Avatars
-    // Borders
-    avatarOnline: {
-        borderWidth: 2,
-        borderColor: Colors.green400,
-    },
+  // Avatars
+  // Borders
+  avatarOnline: {
+    borderWidth: 2,
+    borderColor: Colors.green400,
+  },
 
-    avatarOffline: {
-        borderWidth: 2,
-        borderColor: Colors.orange400,
-    },
+  avatarOffline: {
+    borderWidth: 2,
+    borderColor: Colors.orange400,
+  },
 
-    avatarInvisible: {
-        borderWidth: 2,
-        borderColor: Colors.orange400,
-    },
+  avatarInvisible: {
+    borderWidth: 2,
+    borderColor: Colors.orange400,
+  },
 
-    avatarAnon: {
-        borderWidth: 2,
-        borderColor: Colors.sand300,
-    },
+  avatarAnon: {
+    borderWidth: 2,
+    borderColor: Colors.sand300,
+  },
 
-    // Single avatar
-    // Square
+  // Single avatar
+  // Square
 
-    // Large
-    avatarSquareLarge: {
-        width: 72,
-        height: 72,
-        borderRadius: 8,
-    },
+  // Large
+  avatarSquareLarge: {
+    width: 72,
+    height: 72,
+    borderRadius: 8,
+  },
 
-    // Medium
-    avatarSquareMedium: {
-        width: 48,
-        height: 48,
-        borderRadius: 8,
-    },
+  // Medium
+  avatarSquareMedium: {
+    width: 48,
+    height: 48,
+    borderRadius: 8,
+  },
 
-    // Small
-    avatarSquareSmall: {
-        width: 24,
-        height: 24,
-        borderRadius: 4,
-    },
+  // Small
+  avatarSquareSmall: {
+    width: 24,
+    height: 24,
+    borderRadius: 4,
+  },
 
-    // Circle
+  // Circle
 });
