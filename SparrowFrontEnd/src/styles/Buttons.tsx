e9bdef1b3b3d48e24476194926b8543466c10ca2
annaozola <teesannalv@gmail.com>
2023-12-05 15:58:55 +0200

    Refactor Buttons

--- SparrowFrontEnd/src/styles/Buttons.tsx
+++ SparrowFrontEnd/src/styles/Buttons.tsx
@@ -1,159 +1,152 @@
-import { StyleSheet } from "react-native";
-import { Colors } from "./Colors";
-import { Spacing } from "./Spacing";
+import { StyleSheet } from 'react-native';
+import { Colors } from './Colors';
+import { Spacing } from './Spacing';
 
 export const buttonStyles = StyleSheet.create({
-    // Buttons
-    // Primary
-    buttonPrimaryDark: {
-        backgroundColor: Colors.sparrowDarkBrown,
-        borderColor: Colors.sparrowDarkBrown,
-    },
+  // Buttons
+  // Primary
+  buttonPrimaryDark: {
+    backgroundColor: Colors.sparrowDarkBrown,
+    borderColor: Colors.sparrowDarkBrown,
+  },
 
-    buttonPrimaryLight: {
-        backgroundColor: Colors.sparrowBrown,
-        borderColor: Colors.sparrowBrown,
-    },
+  buttonPrimaryLight: {
+    backgroundColor: Colors.sparrowBrown,
+    borderColor: Colors.sparrowBrown,
+  },
 
-    // Secondary
-    // TODO check this out
-    // buttonSecondaryFill: {
-    //     backgroundColor: Colors.sparrowSand,
-    //     borderColor: Colors.sparrowSand,
-    // },
+  buttonSecondary: {
+    backgroundColor: Colors.sparrowSand,
+    borderColor: Colors.sparrowDarkBrown,
+  },
 
-    buttonSecondary: {
-        backgroundColor: Colors.sparrowSand,
-        borderColor: Colors.sparrowDarkBrown,
-    },
+  // Button Tertiary
+  buttonTertiary: {
+    backgroundColor: Colors.sparrowBrown,
+    borderColor: Colors.sparrowBrown,
+  },
 
-    // Button Tertiary
-    buttonTertiary: {
-        backgroundColor: Colors.sparrowBrown,
-        borderColor: Colors.sparrowBrown,
-    },
+  // Success
+  buttonSuccess: {
+    backgroundColor: Colors.green400,
+    borderColor: Colors.sparrowDarkBrown,
+  },
 
-    // Success
-    buttonSuccess: {
-        backgroundColor: Colors.green400,
-        borderColor: Colors.sparrowDarkBrown,
-    },
+  // Warning
+  buttonWarning: {
+    backgroundColor: Colors.orange400,
+    borderColor: Colors.sparrowDarkBrown,
+  },
 
-    // Warning
-    buttonWarning: {
-        backgroundColor: Colors.orange400,
-        borderColor: Colors.sparrowDarkBrown,
-    },
+  // Error
+  buttonError: {
+    backgroundColor: Colors.red400,
+    borderColor: Colors.sparrowDarkBrown,
+  },
 
-    // Error
-    buttonError: {
-        backgroundColor: Colors.red400,
-        borderColor: Colors.sparrowDarkBrown,
-    },
+  // Function
+  buttonFunction: {
+    backgroundColor: Colors.turqoise300,
+    borderColor: Colors.sparrowDarkBrown,
+  },
 
-    // Function
-    buttonFunction: {
-        backgroundColor: Colors.turqoise300,
-        borderColor: Colors.sparrowDarkBrown
-    },
+  // Layout
+  // Makes the button width the size of the contents
+  buttonContained: {
+    alignSelf: 'flex-start',
+  },
 
-    // Layout
-    // Makes the button width the size of the contents
-    buttonContained: {
-        alignSelf: 'flex-start',
-    },
+  // Makes the button full-width
+  buttonFull: {
+    flex: 1,
+  },
 
-    // Makes the button full-width
-    buttonFull: {
-        flex: 1,
-    },
+  // Icon buttons - buttons with icons
+  // These styles currently work in conjuction
+  iconButtonSmall: {
+    flexDirection: 'row',
+    // TODO going to use spacing.sm instead of md for now cause I think it looks better, update Figma later if this is the final setting
+    columnGap: Spacing.sm,
+  },
 
-    // Icon buttons - buttons with icons
-    // These styles currently work in conjuction
-    iconButtonSmall: {
-        flexDirection: 'row',
-        // TODO going to use spacing.sm instead of md for now cause I think it looks better, update Figma later if this is the final setting
-        columnGap: Spacing.sm,
-    },
+  // TODO check and delete borderColor property if it's unused
 
-    // TODO check and delete borderColor property if it's unused
+  // Sizes
+  // Large
+  // Pair with buttonTextOne
+  textButtonLarge: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    borderWidth: 2,
+    // borderColor: Colors.sparrowDarkBrown,
+  },
 
-    // Sizes
-    // Large
-    // Pair with buttonTextOne
-    textButtonLarge: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 24,
-        paddingHorizontal: 32,
-        borderRadius: 8,
-        borderWidth: 2,
-        // borderColor: Colors.sparrowDarkBrown,
-    },
+  // Medium
+  // Pair with buttonTextOne
+  textButtonMedium: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    borderWidth: 2,
+    // borderColor: Colors.sparrowDarkBrown,
+  },
 
-    // Medium
-    // Pair with buttonTextOne
-    textButtonMedium: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 16,
-        paddingHorizontal: 24,
-        borderRadius: 8,
-        borderWidth: 2,
-        // borderColor: Colors.sparrowDarkBrown,
-    },
+  // Small
+  // Pair with buttonTextTwo
+  textButtonSmall: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderWidth: 2,
+    // borderColor: Colors.sparrowDarkBrown,
+    borderRadius: 8,
+  },
 
-    // Small
-    // Pair with buttonTextTwo
-    textButtonSmall :{
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        borderWidth: 2,
-        // borderColor: Colors.sparrowDarkBrown,
-        borderRadius: 8,
-    },
+  // Extra small
+  // Pair with buttonTextThree
+  textButtonExtraSmall: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    borderWidth: 2,
+    // borderColor: Colors.sparrowDarkBrown,
+    columnGap: Spacing.sm,
+  },
 
-    // Extra small
-    // Pair with buttonTextThree
-    textButtonExtraSmall: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        borderRadius: 8,
-        borderWidth: 2,
-        // borderColor: Colors.sparrowDarkBrown,
-        columnGap: Spacing.sm,
-    },
+  // Button icons
+  // Small
+  buttonIconSmallLight: {
+    fontSize: 24,
+    // Fix for icon being cut off
+    height: 24,
+    color: Colors.sparrowSand,
+  },
 
-    // Button icons
-    // Small
-    buttonIconSmallLight: {
-        fontSize: 24,
-        // Fix for icon being cut off
-        height: 24,
-        color: Colors.sparrowSand,
-    },
+  buttonIconSmallDark: {
+    fontSize: 24,
+    height: 24,
+    color: Colors.sparrowDarkBrown,
+  },
 
-    buttonIconSmallDark: {
-        fontSize: 24,
-        height: 24,
-        color: Colors.sparrowDarkBrown,
-    },
+  // Medium
+  buttonIconMedium: {
+    fontSize: 32,
+    // Fix for icon being cut off
+    height: 32,
+    color: Colors.sparrowSand,
+  },
 
-    // Medium
-    buttonIconMedium: {
-        fontSize: 32,
-        // Fix for icon being cut off
-        height: 32,
-        color: Colors.sparrowSand,
-    },
-
-    buttonIconMediumDark: {
-        fontSize: 32,
-        height: 32,
-        color: Colors.sparrowDarkBrown,
-    },
-})
+  buttonIconMediumDark: {
+    fontSize: 32,
+    height: 32,
+    color: Colors.sparrowDarkBrown,
+  },
+});
