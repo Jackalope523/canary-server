83887451d962eb9fe966b6266fca79e2f693e1c4
annaozola <teesannalv@gmail.com>
2023-12-20 20:18:38 +0200

    Style auth flow landing screen

--- SparrowFrontEnd/src/flows/auth/Landing.tsx
+++ SparrowFrontEnd/src/flows/auth/Landing.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet, Image } from 'react-native';
 import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -12,6 +12,12 @@ import { AuthStackParamList } from '../../components/atoms/types';
 
 import { getAccount } from './accountPigeon';
 import { initialiseAxiosSession } from '../../lib/axios';
+import { globalStyles } from '../../styles/GlobalStyles';
+import TextButton, {
+  TextButtonType,
+  TextButtonVariant,
+} from '../../components/TextButton';
+import { Spacing } from '../../styles/SpacingStyles';
 
 type LandingProps = StackScreenProps<AuthStackParamList, 'Landing'>;
 
@@ -42,28 +48,57 @@ const LandingScreen = ({ navigation }: LandingProps) => {
     navigation.navigate('Login');
   }
 
+  // TODO <Pressable> text button may need to be made into a component named TextButton or something alike
   return (
-    <View>
-      <Text>Sparrow</Text>
-      <Button
-        type={ButtonType.PrimaryDark}
-        size={ButtonSize.Large}
-        display={ButtonDisplay.Contained}
-        btnText={'Sign up'}
-        onPress={signupButton}
-        disabled={!buttonEnabled}
-      />
-
-      <Button
-        type={ButtonType.PrimaryDark}
-        size={ButtonSize.Large}
-        display={ButtonDisplay.Contained}
-        btnText={'Or Log in'}
-        onPress={loginButton}
-        disabled={!buttonEnabled}
-      />
+    <View style={[globalStyles.baseContainer, styles.container]}>
+      <View style={styles.contentWrapper}>
+        <Text style={[globalStyles.headingTextTwo, globalStyles.textDark]}>
+          Sparrow
+        </Text>
+        <Image
+          source={require('../../assets/illustrations/temp/illustration-placeholder.png')}
+          style={globalStyles.illustrationFull}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button
+          type={ButtonType.Success}
+          size={ButtonSize.Medium}
+          display={ButtonDisplay.Full}
+          btnText={'Sign up'}
+          onPress={signupButton}
+          disabled={!buttonEnabled}
+        />
+        <TextButton
+          type={TextButtonType.Dark}
+          variant={TextButtonVariant.Three}
+          btnText="Or log in"
+          onPress={loginButton}
+          disabled={!buttonEnabled}
+        />
+      </View>
     </View>
   );
 };
 
 export default LandingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  contentWrapper: {
+    alignItems: 'center',
+    rowGap: Spacing.lg,
+  },
+
+  buttonWrapper: {
+    alignItems: 'center',
+    width: '100%',
+    rowGap: Spacing.md,
+  },
+});
