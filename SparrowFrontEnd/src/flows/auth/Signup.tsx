6985fd7dc16af304d9d844bc49c859b69ebf7046
annaozola <teesannalv@gmail.com>
2023-12-22 17:20:11 +0200

    Add name, email, date of birth fields

--- SparrowFrontEnd/src/flows/auth/Signup.tsx
+++ SparrowFrontEnd/src/flows/auth/Signup.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import DatePicker from 'react-native-date-picker';
 
@@ -14,6 +14,8 @@ import Button, {
 } from '../../components/Button';
 
 import { signup } from './accountPigeon';
+import TextInputSmall from '../../components/TextInputSmall';
+import { Spacing } from '../../styles/SpacingStyles';
 
 type SignupProps = StackScreenProps<AuthStackParamList, 'Signup'>;
 
@@ -46,35 +48,65 @@ const SignupScreen = ({ navigation }: SignupProps) => {
   }
 
   return (
-    <View>
-      <Text>Phone Number</Text>
-      <TextInput
-        value={PhoneNumber}
-        onChangeText={setPhoneNumber}
-        keyboardType="phone-pad"
-      />
-      <Text>Email</Text>
-      <TextInput value={Email} onChangeText={setEmail} />
-      <Text>Name</Text>
-      <TextInput value={Name} onChangeText={setName} />
-      <Text>Date of Birth</Text>
+    <View style={globalStyles.baseContainer}>
+      <View style={styles.inputSection}>
+        <TextInputSmall
+          label="Name"
+          value={Name}
+          onChangeText={setName}
+          required
+          description="Your name will be public and visible to all users."
+        />
+        <TextInputSmall
+          label="Email"
+          value={Email}
+          onChangeText={setEmail}
+          autoComplete="email"
+          recommended
+          description="We recommend binding an email address to your account in case you change your phone number."
+        />
+        <TextInputSmall
+          label="Date of Birth"
+          value={DateOfBirth}
+          onChangeText={setDateOfBirth}
+          placeholder="MM/DD/YYYY"
+          required
+          description="You must be 18 years or older to use Sparrow. Your date of birth will not be visible to other users."
+        />
+        <TextInputSmall
+          label="Disabled example"
+          placeholder="MM/DD/YYYY"
+          recommended
+          disabled
+        />
+      </View>
+      {/* <Text>Date of Birth</Text>
       <DatePicker
         date={DateOfBirth}
         onDateChange={setDateOfBirth}
         mode="date"
         timeZoneOffsetInMinutes={0}
         maximumDate={new Date()}
-      />
+      /> */}
       <Button
-        type={ButtonType.PrimaryDark}
-        size={ButtonSize.Large}
-        display={ButtonDisplay.Contained}
-        btnText={'Sign up'}
+        type={ButtonType.Success}
+        size={ButtonSize.Medium}
+        display={ButtonDisplay.Full}
+        btnText={'Continue'}
         onPress={navigate}
         disabled={!buttonEnabled}
       />
     </View>
   );
 };
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                     Styles                                     ||
+// ! ||--------------------------------------------------------------------------------||
+const styles = StyleSheet.create({
+  inputSection: {
+    rowGap: Spacing.sm,
+  },
+});
 
 export default SignupScreen;
