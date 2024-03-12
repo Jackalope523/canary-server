78217ce7ea4b4ca751c4870d639c8dc0689eba69
annaozola <teesannalv@gmail.com>
2024-03-12 20:45:32 +0200

    Add a base for Event screen

--- SparrowFrontEnd/src/flows/event/Event.tsx
+++ SparrowFrontEnd/src/flows/event/Event.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
-import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { Colors } from '../../styles/ColorStyles';
 import { globalStyles } from '../../styles/GlobalStyles';
 import { EventStackParamList } from '../../components/atoms/types';
-import Button from '../../components/atoms/Button';
 import { eventShard, getEvent } from './eventPigeon';
 import Avatar, { AvatarSize, AvatarStatus } from '../../components/Avatar';
 import TextLabel, {
@@ -15,18 +21,39 @@ import TextLabel, {
 import { labelText } from '../../components/LabelText';
 import { Spacing } from '../../styles/SpacingStyles';
 import { borderRadius } from '../../styles/BorderStyles';
+import TextButton, {
+  TextButtonType,
+  TextButtonVariant,
+} from '../../components/TextButton';
+import Gallery from '../../components/Gallery';
+import { SAMPLE_PAST_EVENT_DATA } from '../../data/samplePastEventData';
+import Button, {
+  ButtonDisplay,
+  ButtonSize,
+  ButtonType,
+} from '../../components/Button';
 
 type EventProps = StackScreenProps<EventStackParamList, 'Event'>;
 
 // TEMP. images
 import tempAvatar from '../../assets/images/temp/host-img-1.jpg';
 import tempBanner from '../../assets/images/temp/event-img-1.jpg';
+import tempMap from '../../assets/images/temp/temp-map.png';
 
 // Icons
 import DateIcon from '../../assets/icons/date-outline.svg';
 import TimeIcon from '../../assets/icons/time-outline.svg';
+import MapIcon from '../../assets/icons/discovery-fill.svg';
+import LocationIcon from '../../assets/icons/location-outline.svg';
+import PersonIcon from '../../assets/icons/account-outline.svg';
 
 const EventScreen = ({ route }: EventProps) => {
+  const pastEventData = SAMPLE_PAST_EVENT_DATA.find(
+    (event) => event.id === '3',
+  );
+
+  // TODO fix TS error in the code below
+
   // const [errorText, setErrorText] = React.useState('');
   // const [eventText, setEventText] = React.useState('');
 
@@ -50,7 +77,10 @@ const EventScreen = ({ route }: EventProps) => {
   // }
 
   return (
-    <View style={globalStyles.baseContainer}>
+    <ScrollView
+      contentContainerStyle={globalStyles.baseContainer}
+      overScrollMode="never"
+      showsVerticalScrollIndicator={false}>
       {/* <Text style={{ color: Colors.red400 }}>{errorText}</Text>
       <Text>{eventText}</Text> */}
 
@@ -123,8 +153,90 @@ const EventScreen = ({ route }: EventProps) => {
             </View>
           </View>
         </View>
+
+        {/* LOCATION SECTION */}
+        <View style={styles.locationSection}>
+          {/* inner */}
+          <View style={styles.locationInnerSection}>
+            <Text style={[globalStyles.headingTextFour, globalStyles.textDark]}>
+              Location
+            </Text>
+            <TextButton
+              type={TextButtonType.Dark}
+              variant={TextButtonVariant.Four}
+              displayIcon
+              Icon={MapIcon}
+              text="show on maps"
+              onPress={null}
+            />
+          </View>
+
+          <Image source={tempMap} style={styles.mapImage} resizeMode="cover" />
+
+          {/* TODO check how it looks with more than 1 line, might need to adjust stlye; when more than 2 lines don't use alignItems center */}
+          <View style={styles.location}>
+            <LocationIcon
+              width={24}
+              height={24}
+              fill={Colors.sparrowDarkBrown}
+            />
+            <Text style={[globalStyles.bodyTextOne, globalStyles.textDark]}>
+              Location
+            </Text>
+          </View>
+        </View>
+
+        {/* ABOUT SECTION */}
+        <View style={styles.aboutSection}>
+          <Text style={[globalStyles.headingTextFour, globalStyles.textDark]}>
+            About
+          </Text>
+          <Text style={[globalStyles.bodyTextOne, globalStyles.textDark]}>
+            Description
+          </Text>
+        </View>
+
+        {/* ATTENDEES SECTION */}
+        {/* TODO add conditional logic - different TextButtons */}
+        <View style={styles.attendeesSection}>
+          <View style={styles.attendeesInnerSection}>
+            <Text style={[globalStyles.headingTextFour, globalStyles.textDark]}>
+              Attendees
+            </Text>
+            <TextButton
+              type={TextButtonType.Dark}
+              variant={TextButtonVariant.Four}
+              displayIcon
+              Icon={PersonIcon}
+              text="manage"
+              onPress={null}
+            />
+          </View>
+          <Text>AvatarStack component here</Text>
+        </View>
+
+        {/* GALLERY SECTION */}
+        {/* TODO add conditional logic - gallery */}
+        <View style={styles.gallerySection}>
+          {pastEventData?.media.length > 0 && (
+            <Gallery images={pastEventData?.media ? [pastEventData] : []} />
+          )}
+        </View>
+
+        {/* CONTROLS SECTION */}
+        <View style={styles.controlsSection}>
+          <Button
+            type={ButtonType.Error}
+            size={ButtonSize.Medium}
+            display={ButtonDisplay.Full}
+            text={'Continue'}
+            onPress={null}
+          />
+
+          <Text>SmallMessage component here</Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -166,15 +278,67 @@ const styles = StyleSheet.create({
     rowGap: Spacing.sm,
   },
 
-  // TODO column-based layout here (grid)
   dateTimeInnerSection: {
     flexDirection: 'row',
     alignItems: 'center',
+    columnGap: Spacing.md,
+    paddingBottom: Spacing.md,
   },
 
   dateTime: {
     flexDirection: 'row',
     alignItems: 'center',
     columnGap: Spacing.sm,
+    flex: 1,
+  },
+
+  locationSection: {
+    rowGap: Spacing.sm,
+    paddingBottom: Spacing.lg,
+  },
+
+  locationInnerSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  // TODO adjust for mapbox
+  mapImage: {
+    width: '100%',
+    height: 160,
+    borderRadius: borderRadius.md,
+    borderWidth: 2,
+    borderColor: Colors.sparrowDarkBrown,
+  },
+
+  location: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: Spacing.sm,
+  },
+
+  aboutSection: {
+    rowGap: Spacing.sm,
+    paddingBottom: Spacing.lg,
+  },
+
+  attendeesSection: {
+    rowGap: Spacing.sm,
+    paddingBottom: Spacing.lg,
+  },
+
+  attendeesInnerSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  gallerySection: {
+    paddingBottom: Spacing.xl,
+  },
+
+  controlsSection: {
+    rowGap: Spacing.md,
   },
 });
