80f9059df198dcaf699dd55709e7331675d3fc93
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2024-04-03 14:41:03 -0400

    Add Media operations boundary

--- Hollow/Core/CoreTerminal.cs
+++ Hollow/Core/CoreTerminal.cs
@@ -14,12 +14,13 @@ namespace Core
         private static object initLock = new();
 
         public IAccountDatabase AccountDatabase { get; init; }
+        public IAdminDatabase AdminDatabase { get; init; }
         public IEventDatabase EventDatabase { get; init; }
         public IEtchingDatabase EtchingDatabase { get; init; }
-        public IProfileDatabase ProfileDatabase { get; init; }
         public IDisciplineDatabase DisciplineDatabase { get; init; }
+        public IImageDatabase ImageDatabase { get; init; }
         public INotificationDatabase NotificationDatabase { get; init; }
-        public IAdminDatabase AdminDatabase { get; init; }
+        public IProfileDatabase ProfileDatabase { get; init; }
 
         public IAccountOperations AccountOperations
             => AccountDirector;
@@ -27,64 +28,73 @@ namespace Core
             => EventDirector;
         public IEtchingOperations EtchingOperations
             => EtchingDirector;
-
-        public IProfileOperations ProfileOperations
-            => ProfileDirector;
         public IDisciplineOperations DisciplineOperations
             => DisciplineDirector;
+        public IMediaOperations MediaOperations
+            => MediaDirector;
         public INotificationOperations NotificationOperations
             => NotificationDirector;
+        public IProfileOperations ProfileOperations
+            => ProfileDirector;
 
         public INotificationService NotificationService { get; init; }
 
         internal AccountDirector AccountDirector { get; private set; }
         internal EventDirector EventDirector { get; private set; }
         internal EtchingDirector EtchingDirector { get; private set; }
-        internal ProfileDirector ProfileDirector { get; private set; }
         internal DisciplineDirector DisciplineDirector { get; private set; }
+        internal MediaDirector MediaDirector { get; private set; }
         internal NotificationDirector NotificationDirector { get; private set; }
+        internal ProfileDirector ProfileDirector { get; private set; }
 
         public List<(Type GateType, object Instance)> Gates
             => new() { (typeof(IAccountOperations), AccountOperations),
                 (typeof(IEventOperations), EventOperations),
                 (typeof(IEtchingOperations), EtchingOperations),
-                (typeof(IProfileOperations), ProfileOperations),
                 (typeof(IDisciplineOperations), DisciplineOperations),
-                (typeof(INotificationOperations), NotificationOperations) };
+                (typeof(IMediaOperations), MediaOperations),
+                (typeof(INotificationOperations), NotificationOperations),
+                (typeof(IProfileOperations), ProfileOperations) };
 
         #endregion
 
         #region Initialisation
 
-        public static CoreTerminal CreateTerminal(IAccountDatabase accountDatabase, IEventDatabase eventDatabase,
-            IEtchingDatabase etchingDatabase, IProfileDatabase profileDatabase,
-            IDisciplineDatabase disciplineDatabase, INotificationDatabase notificationDatabase,
-            IAdminDatabase adminDatabase, INotificationService notificationService)
+        public static CoreTerminal CreateTerminal(IAccountDatabase accountDatabase, IAdminDatabase adminDatabase,
+            IEventDatabase eventDatabase, IEtchingDatabase etchingDatabase,
+            IDisciplineDatabase disciplineDatabase, IImageDatabase imageDatabase,
+            INotificationDatabase notificationDatabase, IProfileDatabase profileDatabase,
+            INotificationService notificationService)
         {
             lock (initLock)
             {
                 if (Terminal == null)
                 {
-                    Terminal = new CoreTerminal(accountDatabase, eventDatabase, etchingDatabase, profileDatabase,
-                    disciplineDatabase, notificationDatabase, adminDatabase, notificationService);
+                    Terminal = new CoreTerminal(accountDatabase, adminDatabase,
+                        eventDatabase, etchingDatabase,
+                        disciplineDatabase, imageDatabase,
+                        notificationDatabase, profileDatabase,
+                        notificationService);
                 }
 
                 return Terminal;
             }
         }
 
-        private CoreTerminal(IAccountDatabase accountDatabase, IEventDatabase eventDatabase,
-            IEtchingDatabase etchingDatabase, IProfileDatabase profileDatabase,
-            IDisciplineDatabase disciplineDatabase, INotificationDatabase notificationDatabase,
-            IAdminDatabase adminDatabase, INotificationService notificationService)
+        private CoreTerminal(IAccountDatabase accountDatabase, IAdminDatabase adminDatabase,
+			IEventDatabase eventDatabase, IEtchingDatabase etchingDatabase,
+			IDisciplineDatabase disciplineDatabase, IImageDatabase imageDatabase,
+			INotificationDatabase notificationDatabase, IProfileDatabase profileDatabase,
+            INotificationService notificationService)
         {
             AccountDatabase = accountDatabase;
+            AdminDatabase = adminDatabase;
             EventDatabase = eventDatabase;
             EtchingDatabase = etchingDatabase;
-            ProfileDatabase = profileDatabase;
             DisciplineDatabase = disciplineDatabase;
+            ImageDatabase = imageDatabase;
             NotificationDatabase = notificationDatabase;
-            AdminDatabase = adminDatabase;
+            ProfileDatabase = profileDatabase;
 
             NotificationService = notificationService;
 
@@ -96,9 +106,10 @@ namespace Core
             AccountDirector = new AccountDirector(this);
             EventDirector = new EventDirector(this);
             EtchingDirector = new EtchingDirector(this);
-            ProfileDirector = new ProfileDirector(this);
             DisciplineDirector = new DisciplineDirector(this);
+            MediaDirector = new MediaDirector(this);
             NotificationDirector = new NotificationDirector(this);
+            ProfileDirector = new ProfileDirector(this);
         }
 
         #endregion
