bcda602fc204003cb12a0fdf3ce0039e65f0d384
annaozola <teesannalv@gmail.com>
2024-04-04 13:03:29 +0300

    Update Avatar component and remove statuses

--- SparrowFrontEnd/src/components/EventCardLarge.tsx
+++ SparrowFrontEnd/src/components/EventCardLarge.tsx
@@ -1,17 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import Avatar, { AvatarSize } from './Avatar';
+import Avatar, { AvatarSize, AvatarStatus } from './Avatar';
 
 interface EventCardLargeProps {}
+
+// Icons
+import FeatherIcon from '../assets/icons/feather-fill-colored.svg';
+
+// TODO delete later after updating; TEMP. imports
+import TempAvatarImage from '../assets/images/temp/host-img-1.jpg';
+
+import { globalStyles } from '../styles/GlobalStyles';
 
 const EventCardLarge: React.FC<EventCardLargeProps> = () => {
   return (
     <View style={styles.eventCardLarge}>
       <View style={styles.host}>
-        <Avatar size={AvatarSize.Small} />
+        <Avatar size={AvatarSize.Small} image={TempAvatarImage} />
         <View style={styles.hostNameContainer}>
-          <Text>Name</Text>
-          <Text>Friend icon</Text>
+          <Text style={[globalStyles.headingTextFour, globalStyles.textDark]}>
+            Host name
+          </Text>
+          <FeatherIcon height={24} width={24} />
         </View>
       </View>
     </View>
