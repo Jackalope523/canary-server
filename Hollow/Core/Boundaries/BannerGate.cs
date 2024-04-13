f9ffbed3d3dd4cfd050c4ed4d9f8d82b086ea786
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2024-04-13 13:48:33 -0400

    Add banner gate

---
+++ Hollow/Core/Boundaries/BannerGate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Boundaries
+{
+	#region Schema
+
+	#endregion
+
+	#region Gates
+
+	public interface IBannerDatabase
+	{
+        Task<string> GetUserBannerAsync(ulong userId);
+        Task<string> GetUserBannerAsync(string phoneNumber);
+        Task AddBannerMemberAsync(string phoneNumber);
+    }
+
+	public interface IBannerOperations
+	{
+        Task InviteUserAsync(ulong userId, string invitedPhoneNumber);
+    }
+
+    #endregion
+}
+
