82d44ad6e21087656f18bb3b7db4f546cc812a23
EthanCote5233 <ECote523@Gmail.com>
2024-04-30 15:05:38 -0400

    Begin implementing Banner Store

---
+++ Repository/Coordinators/BannerStoreCoordinator.cs
@@ -0,0 +1,27 @@
+﻿namespace Repository
+{
+    internal class BannerStoreCoordinator : IBannerDatabase
+    {
+        private readonly IBannerDatabase store;
+
+        public BannerStoreCoordinator(Harbor.Flag flag)
+        {
+            store = new EFCoreBannerStore(flag);
+        }
+
+        public async Task AddBannerMemberAsync(string phoneNumber, string banner)
+        {
+            await store.AddBannerMemberAsync(phoneNumber, banner);
+        }
+
+        public async Task<string> GetUserBannerAsync(ulong userId)
+        {
+            return await store.GetUserBannerAsync(userId);
+        }
+
+        public async Task<string> GetUserBannerAsync(string phoneNumber)
+        {
+            return await store.GetUserBannerAsync(phoneNumber);
+        }
+    }
+}
---
+++ Repository/Databases/EFCore/Stores/EFCoreBannerStore.cs
@@ -0,0 +1,24 @@
+﻿namespace Repository
+{
+    public class EFCoreBannerStore : QueryStore, IBannerDatabase
+    {
+        public EFCoreBannerStore(Harbor.Flag flag) : base(flag)
+        {
+        }
+
+        public async Task AddBannerMemberAsync(string phoneNumber, string banner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetUserBannerAsync(ulong userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetUserBannerAsync(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
