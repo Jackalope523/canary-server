cd38ec35d40be51708b3658bf04d2991ddaaba85
Ethan Côté <72030426+Jackalope523@users.noreply.github.com>
2024-10-10 18:05:37 -0400

    Update 20240725202329_v3.Designer.cs

--- Repository/Databases/EFCore/Migrations/StagingMigrations/20240725202329_v3.Designer.cs
+++ Repository/Databases/EFCore/Migrations/StagingMigrations/20240725202329_v3.Designer.cs
@@ -12,7 +12,7 @@ using Repository;
 
 namespace Repository.Databases.EFCore.Migrations.AzureMigrations
 {
-    [DbContext(typeof(AzureProductionContext))]
+    [DbContext(typeof(AzureStagingContext))]
     [Migration("20240725202329_v3")]
     partial class v3
     {
