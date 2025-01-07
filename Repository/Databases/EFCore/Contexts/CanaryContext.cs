d393c21d19fcdccd898ccc8e09432c2f477de082
EthanCote5233 <ECote523@Gmail.com>
2025-01-07 18:32:00 -0500

    Implement notifications

--- Repository/Databases/EFCore/Contexts/CanaryContext.cs
+++ Repository/Databases/EFCore/Contexts/CanaryContext.cs
@@ -1,6 +1,4 @@
-﻿using Azure.Security.KeyVault.Certificates;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
 using Repository.Entities;
 
 namespace Repository
@@ -23,6 +21,7 @@ namespace Repository
         internal DbSet<BannerLink> BannerLinks { get; set; }
         internal DbSet<GuestClearance> GuestClearances { get; set; }
         internal DbSet<Feedback> Feedback { get; set; }
+        internal DbSet<Notification> Notifications { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -31,7 +30,7 @@ namespace Repository
 
             // User
             modelBuilder.Entity<User>()
-            .HasQueryFilter(u => !u.SoftDeleted);
+                .HasQueryFilter(u => !u.SoftDeleted);
 
             modelBuilder.Entity<User>()
                 .HasData(new User()
@@ -51,42 +50,51 @@ namespace Repository
                    IsPhoneConfirmed = true,
                });
 
-            modelBuilder.Entity<User>().Property(u => u.Email)
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
                 .HasMaxLength(255);
 
-            modelBuilder.Entity<User>().Property(u => u.Name)
+            modelBuilder.Entity<User>()
+                .Property(u => u.Name)
                 .HasMaxLength(100);
 
-            modelBuilder.Entity<User>().Property(u => u.Pseudonym)
-               .HasMaxLength(100);
+            modelBuilder.Entity<User>()
+                .Property(u => u.Pseudonym)
+                .HasMaxLength(100);
 
-            modelBuilder.Entity<User>().Property(u => u.NormalisedEmail)
+            modelBuilder.Entity<User>()
+                .Property(u => u.NormalisedEmail)
                 .HasMaxLength(255);
 
-            modelBuilder.Entity<User>().Property(u => u.PhoneNumber)
+            modelBuilder.Entity<User>()
+                .Property(u => u.PhoneNumber)
                 .HasMaxLength(20);
 
-            modelBuilder.Entity<User>().Property(u => u.SecurityStamp)
+            modelBuilder.Entity<User>()
+                .Property(u => u.SecurityStamp)
                 .HasMaxLength(50);
 
-            modelBuilder.Entity<User>().Property(u => u.Haunt)
-                .HasSrid(4326);
-
-            modelBuilder.Entity<User>().Property(u => u.CurrentLocation)
-                .HasSrid(4326);
-
-            modelBuilder.Entity<Gathering>().Property(e => e.Location)
+            modelBuilder.Entity<User>()
+                .Property(u => u.Haunt)
                 .HasSrid(4326);
 
             modelBuilder.Entity<User>()
-                 .HasMany(u => u.HostedGatherings)
-                 .WithOne(g => g.Host)
-                 .OnDelete(DeleteBehavior.Restrict);
+                .Property(u => u.CurrentLocation)
+                .HasSrid(4326);
+
+            modelBuilder.Entity<Gathering>()
+                .Property(e => e.Location)
+                .HasSrid(4326);
 
             modelBuilder.Entity<User>()
-                 .HasMany(u => u.InitiatedUserRelationships)
-                 .WithOne(l => l.Self)
-                 .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.HostedGatherings)
+                .WithOne(g => g.Host)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.InitiatedUserRelationships)
+                .WithOne(l => l.Self)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
                 .HasMany(u => u.TargetUserRelationships)
@@ -129,193 +137,210 @@ namespace Repository
                 .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-               .HasMany(u => u.GatheringReports)
-               .WithOne(r => r.User)
-               .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.GatheringReports)
+                .WithOne(r => r.User)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-               .HasMany(u => u.Snapshots)
-               .WithOne(r => r.Owner)
-               .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.Snapshots)
+                .WithOne(r => r.Owner)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-               .HasMany(u => u.Subscriptions)
-               .WithOne(s => s.User)
-               .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.Subscriptions)
+                .WithOne(s => s.User)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-             .HasMany(u => u.Feedback)
-             .WithOne(f => f.User)
-             .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.Feedback)
+                .WithOne(f => f.User)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-             .HasMany(u => u.Penalties)
-             .WithOne(p => p.Penalized)
-             .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.Penalties)
+                .WithOne(p => p.Penalized)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-             .HasMany(u => u.GuestClearances)
-             .WithOne(c => c.User)
-             .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.GuestClearances)
+                .WithOne(c => c.User)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<User>()
-            .HasMany(u => u.BannerLinks)
-            .WithOne(l => l.User)
-            .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(u => u.BannerLinks)
+                .WithOne(l => l.User)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // Gathering
             modelBuilder.Entity<Gathering>()
-            .HasQueryFilter(g => !g.SoftDeleted);
+                .HasQueryFilter(g => !g.SoftDeleted);
 
-            modelBuilder.Entity<Gathering>().Property(g => g.Description)
+            modelBuilder.Entity<Gathering>()
+                .Property(g => g.Description)
                 .HasMaxLength(1000);
 
-            modelBuilder.Entity<Gathering>().Property(g => g.FriendlyLocation)
+            modelBuilder.Entity<Gathering>()
+                .Property(g => g.FriendlyLocation)
                 .HasMaxLength(255);
 
-            modelBuilder.Entity<Gathering>().Property(g => g.Title)
+            modelBuilder.Entity<Gathering>()
+                .Property(g => g.Title)
                 .HasMaxLength(100);
 
-            modelBuilder.Entity<Gathering>().Property(g => g.Location)
+            modelBuilder.Entity<Gathering>()
+                .Property(g => g.Location)
                 .HasSrid(4326);
 
             modelBuilder.Entity<Gathering>()
-               .HasMany(g => g.GatheringLink)
-               .WithOne(l => l.Gathering)
-               .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(g => g.GatheringLink)
+                .WithOne(l => l.Gathering)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Gathering>()
-              .HasMany(g => g.Snapshots)
-              .WithOne(s => s.Gathering)
-              .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(g => g.Snapshots)
+                .WithOne(s => s.Gathering)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Gathering>()
-              .HasMany(g => g.GatheringReports)
-              .WithOne(l => l.Gathering)
-              .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(g => g.GatheringReports)
+                .WithOne(l => l.Gathering)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Gathering>()
-             .HasMany(g => g.GuestClearances)
-             .WithOne(c => c.Gathering)
-             .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(g => g.GuestClearances)
+                .WithOne(c => c.Gathering)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Gathering>()
-             .HasMany(g => g.UserReports)
-             .WithOne(r => r.Gathering)
-             .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(g => g.UserReports)
+                .WithOne(r => r.Gathering)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // Telegram
             modelBuilder.Entity<Telegram>()
-            .HasQueryFilter(t => !t.SoftDeleted);
-
-            modelBuilder.Entity<Telegram>().Property(n => n.Action)
-               .HasMaxLength(500);
+                .HasQueryFilter(t => !t.SoftDeleted);
+                
+            modelBuilder.Entity<Telegram>()
+                .Property(n => n.Action)
+                .HasMaxLength(500);
 
             // Subscription
             modelBuilder.Entity<Subscription>()
-            .HasQueryFilter(s => !s.SoftDeleted);
+                .HasQueryFilter(s => !s.SoftDeleted);
 
-            modelBuilder.Entity<Subscription>().Property(s => s.DeviceToken)
-              .HasMaxLength(500);
+            modelBuilder.Entity<Subscription>()
+                .Property(s => s.DeviceToken)
+                .HasMaxLength(500);
 
             // User Report
             modelBuilder.Entity<UserReport>()
-            .HasQueryFilter(r => !r.SoftDeleted);
+                .HasQueryFilter(r => !r.SoftDeleted);
 
-            modelBuilder.Entity<UserReport>().Property(r => r.Notes)
-              .HasMaxLength(2000);
+            modelBuilder.Entity<UserReport>()
+                .Property(r => r.Notes)
+                .HasMaxLength(2000);
 
             // Gathering Report
             modelBuilder.Entity<GatheringReport>()
-            .HasQueryFilter(r => !r.SoftDeleted);
+                .HasQueryFilter(r => !r.SoftDeleted);
 
-            modelBuilder.Entity<GatheringReport>().Property(r => r.Notes)
-              .HasMaxLength(2000);
+            modelBuilder.Entity<GatheringReport>()
+                .Property(r => r.Notes)
+                .HasMaxLength(2000);
 
             // Snapshot Report
             modelBuilder.Entity<SnapshotReport>()
-            .HasQueryFilter(r => !r.SoftDeleted);
+                .HasQueryFilter(r => !r.SoftDeleted);
 
-            modelBuilder.Entity<SnapshotReport>().Property(r => r.Notes)
-              .HasMaxLength(2000);
+            modelBuilder.Entity<SnapshotReport>()
+                .Property(r => r.Notes)
+                .HasMaxLength(2000);
 
             // Snapshot
             modelBuilder.Entity<Snapshot>()
-            .HasQueryFilter(s => !s.SoftDeleted);
+                .HasQueryFilter(s => !s.SoftDeleted);
 
             modelBuilder.Entity<Snapshot>()
-               .HasMany(s => s.Reports)
-               .WithOne(r => r.Snapshot)
-               .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(s => s.Reports)
+                .WithOne(r => r.Snapshot)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Snapshot>()
-              .HasMany(s => s.SnapshotLinks)
-              .WithOne(l => l.Snapshot)
-              .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(s => s.SnapshotLinks)
+                .WithOne(l => l.Snapshot)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // Banner
             modelBuilder.Entity<Banner>()
-            .HasQueryFilter(b => !b.SoftDeleted);
+                .HasQueryFilter(b => !b.SoftDeleted);
 
             modelBuilder.Entity<Banner>().Property(b => b.Name)
-             .HasMaxLength(100);
+                .HasMaxLength(100);
 
             modelBuilder.Entity<Banner>().Property(b => b.Description)
-             .HasMaxLength(1000);
+                .HasMaxLength(1000);
 
             modelBuilder.Entity<Banner>().Property(b => b.Code)
-             .HasMaxLength(50);
+                .HasMaxLength(50);
 
             modelBuilder.Entity<Banner>().Property(b => b.Color)
-            .HasMaxLength(7);
+                .HasMaxLength(7);
 
             modelBuilder.Entity<Banner>()
-              .HasMany(b => b.Links)
-              .WithOne(l => l.Banner)
-              .OnDelete(DeleteBehavior.Restrict);
+                .HasMany(b => b.Links)
+                .WithOne(l => l.Banner)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // Feedback
             modelBuilder.Entity<Feedback>()
-            .HasQueryFilter(f => !f.SoftDeleted);
+                .HasQueryFilter(f => !f.SoftDeleted);
 
             modelBuilder.Entity<Feedback>().Property(f => f.Comments)
-            .HasMaxLength(300);
+                .HasMaxLength(300);
 
             // Snapshot Link
             modelBuilder.Entity<SnapshotLink>()
-            .HasQueryFilter(l => !l.SoftDeleted);
+                .HasQueryFilter(l => !l.SoftDeleted);
 
             modelBuilder.Entity<SnapshotLink>()
-               .HasIndex(l => new { l.UserId, l.SnapshotId })
-               .IsUnique();;
+                .HasIndex(l => new { l.UserId, l.SnapshotId })
+                .IsUnique();;
 
             // User Relationship
             modelBuilder.Entity<UserRelationship>()
-            .HasQueryFilter(r => !r.SoftDeleted);
+                .HasQueryFilter(r => !r.SoftDeleted);
 
             modelBuilder.Entity<UserRelationship>()
-               .HasIndex(l => new { l.SelfId, l.OtherId })
-               .IsUnique();
+                .HasIndex(l => new { l.SelfId, l.OtherId })
+                .IsUnique();
 
             // Gathering Link
             modelBuilder.Entity<GatheringLink>()
-            .HasQueryFilter(l => !l.SoftDeleted);
+                .HasQueryFilter(l => !l.SoftDeleted);
 
             // Banner Link
             modelBuilder.Entity<BannerLink>()
-            .HasQueryFilter(l => !l.SoftDeleted);
+                .HasQueryFilter(l => !l.SoftDeleted);
 
             // Guest Clearance
             modelBuilder.Entity<GuestClearance>()
-            .HasQueryFilter(c => !c.SoftDeleted);
+                .HasQueryFilter(c => !c.SoftDeleted);
 
             modelBuilder.Entity<GuestClearance>()
-               .HasIndex(l => new { l.UserId, l.GatheringId})
-               .IsUnique();
+                .HasIndex(l => new { l.UserId, l.GatheringId})
+                .IsUnique();
 
             // Penalty
             modelBuilder.Entity<Penalty>()
-            .HasQueryFilter(p => !p.SoftDeleted);
+                .HasQueryFilter(p => !p.SoftDeleted);
+
+            // Notifications
+            modelBuilder.Entity<Notification>()
+                .HasQueryFilter(n => !n.SoftDeleted);
+
+            modelBuilder.Entity<Notification>()
+                .Property(n => n.NotificationId)
+                .HasMaxLength(36);
         }
     }
 }
--- Repository/Databases/EFCore/Stores/EFCoreNotificationStore.cs
+++ Repository/Databases/EFCore/Stores/EFCoreNotificationStore.cs
@@ -1,7 +1,6 @@
-﻿using Core.Boundaries;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Repository.Entities;
-
+using static Repository.Notification;
 
 namespace Repository
 {
@@ -46,6 +45,161 @@ namespace Repository
             };
 
             await storeSentry.ExecuteWriteAsync(ctx => ctx.Telegrams.Add(toAdd));
+        }
+
+        public Task<NotificationProfile> GetNotificationProfileAsync(long userId)
+        {
+            return storeSentry.ExecuteReadAsync(ctx => 
+                ctx.Users.
+                Where(u => u.Id == userId).
+                Select(u => new NotificationProfile(
+                    u.Id, 
+                    u.NotificationId, 
+                    u.SocialInvitations,
+                    u.CompanionActivity,
+                    u.GatheringReminders, 
+                    u.GatheringActivity, 
+                    u.GatheringDiscovery
+                )).SingleAsync());
+        }
+
+        public async Task UpdateNotificationProfileAsync(long userId, List<(string Property, object Value)> edits)
+        {
+            Discussion currentDiscussion = storeSentry.BeginDiscussion();
+
+            User u = new() { Id = userId };
+
+            storeSentry.DiscussWrite(ctx => ctx.Users.Attach(u), currentDiscussion);
+
+            foreach ((string Property, object Value) in edits)
+            {
+                switch (Property)
+                {
+                    case nameof(NotificationProfile.SocialInvitation):
+                        u.SocialInvitations = (bool)Value;
+                        break;
+                    case nameof(NotificationProfile.CompanionActivity):
+                        u.CompanionActivity = (bool)Value;
+                        break;
+                    case nameof(NotificationProfile.GatheringReminder):
+                        u.GatheringReminders = (bool)Value;
+                        break;
+                    case nameof(NotificationProfile.GatheringActivity):
+                        u.GatheringActivity = (bool)Value;
+                        break;
+                    case nameof(NotificationProfile.GatheringDiscovery):
+                        u.GatheringDiscovery = (bool)Value;
+                        break;
+                    default:
+                        throw new InvalidInputException("Property named \"" + Property + "\" can not be updated using this method.");
+                }
+                storeSentry.DiscussWrite(ctx => ctx.Entry(u).Property(Property).IsModified = true, currentDiscussion);
+            }
+            await storeSentry.EndDiscussionAsync(currentDiscussion);
+        }
+
+        public async Task ClearGatheringNotificationScheduleAsync(long gatheringId)
+        {
+            await storeSentry.ExecuteWriteAsync(ctx =>
+                ctx.Notifications.
+                Where(n => n.GatheringId == gatheringId).
+                ExecuteDeleteAsync());
+        }
+
+        public async Task<(HostNotificationSchedule, List<GuestNotificationSchedule>)> GetGatheringNotificationScheduleAsync(long gatheringId)
+        {
+            List<Notification> notifications = await storeSentry.ExecuteReadAsync(ctx =>
+                                                ctx.Notifications.
+                                                Where(n => n.GatheringId == gatheringId).
+                                                ToListAsync());
+
+            HostNotificationSchedule hostNotification = new("ERROR");
+            Dictionary<long, string> guestUpcomingNotifications = new();
+            Dictionary<long, string> guestImminentNotifications = new();
+
+            foreach (Notification notification in notifications) 
+            {
+                switch (notification.Type)
+                {
+                    case NotificationType.GatheringWaiting:
+                        hostNotification = new(notification.NotificationId);
+                        break;
+                    case NotificationType.GatheringUpcoming:
+                        guestUpcomingNotifications.Add(notification.RecipientId, notification.NotificationId);
+                        break;
+                    case NotificationType.GatheringImminent:
+                        guestImminentNotifications.Add(notification.RecipientId, notification.NotificationId);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            List<GuestNotificationSchedule> guestNotifications = new();
+            foreach (long id in guestUpcomingNotifications.Keys)
+            {
+                guestNotifications.Add(new(id, guestUpcomingNotifications[id], guestImminentNotifications[id]));
+            }
+
+            return (hostNotification, guestNotifications);
+        }
+
+        public async Task UpdateGatheringGuestNotificationSchedulesAsync(long gatheringId, params (long userId, string gatheringUpcomingId, string gatheringImminentId)[] guestSchedules)
+        {
+            await storeSentry.ExecuteWriteAsync(ctx => 
+                ctx.Notifications.
+                Where(n => n.GatheringId == gatheringId && n.Type != NotificationType.GatheringWaiting).
+                ExecuteDeleteAsync());
+
+            Discussion discussion = storeSentry.BeginDiscussion();
+
+            foreach (var schedule in guestSchedules) 
+            {
+                Notification upcoming = new()
+                {
+                    GatheringId = gatheringId,
+                    RecipientId = schedule.userId,
+                    NotificationId = schedule.gatheringUpcomingId,
+                    Type = NotificationType.GatheringUpcoming,
+                };
+
+                Notification imminent = new()
+                {
+                    GatheringId = gatheringId,
+                    RecipientId = schedule.userId,
+                    NotificationId = schedule.gatheringImminentId,
+                    Type = NotificationType.GatheringImminent,
+                };
+
+                storeSentry.DiscussWrite(ctx => ctx.Notifications.AddRange(upcoming, imminent), discussion);
+            }
+
+            await storeSentry.EndDiscussionAsync(discussion);
+        }
+
+        public async Task UpdateGatheringHostNotificationScheduleAsync(long gatheringId, string gatheringWaitingId)
+        {
+            await storeSentry.ExecuteWriteAsync(ctx =>
+               ctx.Notifications.
+               Where(n => n.GatheringId == gatheringId && n.Type == NotificationType.GatheringWaiting).
+               ExecuteDeleteAsync());
+
+            long? hostId = await storeSentry.ExecuteReadAsync(ctx => 
+                            ctx.Gatherings.
+                            Where(g => g.Id == gatheringId).
+                            Select(g => g.HostId).
+                            SingleAsync());
+
+            await storeSentry.ExecuteWriteAsync(ctx =>
+               ctx.Notifications.
+               Add(new() 
+               { 
+                   RecipientId = hostId ?? 0,
+                   GatheringId = gatheringId, 
+                   NotificationId = gatheringWaitingId, 
+                   Type = NotificationType.GatheringWaiting
+                }
+            ));
         }
     }
 }
