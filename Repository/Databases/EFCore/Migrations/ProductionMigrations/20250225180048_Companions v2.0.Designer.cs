42b8d60355c37fcaa0300f70104aa837560ff2af
EthanCote5233 <ECote523@Gmail.com>
2025-03-03 13:59:05 -0500

    Add migration

--- Repository/Databases/EFCore/Migrations/ProductionMigrations/20250225180048_Companions v2.0.Designer.cs
+++ Repository/Databases/EFCore/Migrations/ProductionMigrations/20250225180048_Companions v2.0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Repository;
@@ -12,9 +13,11 @@ using Repository;
 namespace Repository.Databases.EFCore.Migrations.ProductionMigrations
 {
     [DbContext(typeof(AzureProductionContext))]
-    partial class AzureProductionContextModelSnapshot : ModelSnapshot
+    [Migration("20250225180048_Companions v2.0")]
+    partial class Companionsv20
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
